5d02255c8b193acbece9f1f37285513b53c1bc49
Platon <platon.tikhnenko@gmail.com>
2025-12-01 19:08:04 -0600

    half done with register route and succesufully connected to mongodb

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from validates.validate_api import validate_route
 from validates.validate_db import validate_db_data
 from extensions.mongo import mongo
+from db_schemas.users import users_schema
+from validates.validate_db import validate_db_data
 
 load_dotenv()
 
@@ -10,10 +12,28 @@ auth_bl = Blueprint("auth_bl", __name__)
 
 @auth_bl.route("/register", methods=["GET", "POST"])
 def register():
-    data = validate_route(request, "register")
-    if "error" in data:
-        return {"message": data}
+    if request.method == "POST":
+        data = validate_route(request, "register")
+        if "error" in data:
+            return {"message": data}, 400
+        db_data = {
+            "username": data.get("username"),
+            "password": data.get("password"),
+            "email": data.get("email"),
+            "account_type": data.get("account_type")
+        }
+        db_validated_data = validate_db_data(db_data, users_schema)
+        if "error" in db_validated_data:
+            return {"message": db_validated_data}, 400
+
+        duplicated_user = mongo.db.users.find_one({"username": data.get("username")})
+        if duplicated_user:
+            return {"message": "username is already taken"}, 400
+        
+        mongo.db.users.insert_one(db_validated_data)
+
+        return {"message": f"user: {data.get("username")} has created an account"}
     
-    
+
     
     
--- service.py
+++ service.py
@@ -11,10 +11,14 @@ def create_service():
     app.config["MONGO_URI"] = os.getenv("MONGO")
     app.secret_key = os.getenv("APP_SECRET")
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXP"))
-    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(hours=os.getenv("REFRESH_TOKEN_EXP"))
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=10)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(hours=24)
 
     mongo.init_app(app)
+
+    from blueprints.auth.routes import auth_bl
+    
+    app.register_blueprint(auth_bl, url_prefix='/auth')
 
 
     return app
