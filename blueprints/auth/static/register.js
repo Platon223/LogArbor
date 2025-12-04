32cb2af9f29e12007f673f228285c42d0fc45350
Platon <platon.tikhnenko@gmail.com>
2025-12-03 18:12:09 -0600

    fetched the endpoint with js on frontend

--- blueprints/auth/static/register.js
+++ blueprints/auth/static/register.js
@@ -15,13 +15,41 @@ class Register {
             account_type: this.account_type
         }
 
+        try{
+            const response = await fetch("/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(json)
+            })
 
-        const response = await fetch("/auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(json)
-        })
+            if (!response.ok) {
+                return `HTTP error while creating an account: ${response.status}`
+            }
+
+            const data = await response.json()
+
+            return `${data.message}`
+        } catch(error) {
+            return `error: ${error}`
+        }
     }
 }
+
+const formDiv = document.querySelector(".auth-form")
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const account_type = document.querySelector(".select-input")
+
+formDiv.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const registerClass = new Register(username.value, password.value, email.value, account_type.value)
+    const submit = registerClass.submit()
+
+    if (submit.includes("error")) {
+        console.log("error occured")
+    }
+})
