df8f71c10d8f021b16474a6e77465dbf96e6c829
Platon <platon.tikhnenko@gmail.com>
2025-12-19 19:13:41 -0600

    fixed the jwt required function

--- blueprints/home/routes.py
+++ blueprints/home/routes.py
@@ -4,12 +4,12 @@ from flask_jwt_extended import jwt_required
 home_blp = Blueprint("home_blp", __name__, template_folder="templates", static_folder="static")
 
 @home_blp.route("/dashboard", methods=["GET"])
-@jwt_required
+@jwt_required()
 def dashboard():
     return render_template("dashboard.html")
 
 
 @home_blp.route("/credentials/username", methods=["POST"])
-@jwt_required
+@jwt_required()
 def username_info():
     pass
