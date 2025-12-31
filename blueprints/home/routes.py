2d84b09e875446a7808599db32d7cad30c76d1b3
Platon <platon.tikhnenko@gmail.com>
2025-12-31 11:46:12 -0600

    updated the home_bl to log to LogArbor

--- blueprints/home/routes.py
+++ blueprints/home/routes.py
@@ -34,8 +34,7 @@ def username_info():
         log("DASHBOARD", "warning", "user was not found at credentials/username")
         return {"message": "user not found"}, 404
     
-    log_output = loggg("e2e48ac7-0913-4d47-b061-0ca4e8ab4a1a", "INFO", "user got their credentials successufully")
-    log("DASHBOARD", "info", f"{log_output}")
+    loggg("e2e48ac7-0913-4d47-b061-0ca4e8ab4a1a", "info", "user got their credentials successufully")
     return {"message": current_user["username"]}, 200
 
 
