e2b6f8cfc6e5c1572b569e5e526f6f0192aea12e
Platon <platon.tikhnenko@gmail.com>
2026-01-20 17:18:35 -0600

    removed the log from the exception handler in settings bl

--- blueprints/settings/routes.py
+++ blueprints/settings/routes.py
@@ -42,13 +42,6 @@ def handle_operation_failure_pymongo(e):
 
 @settings_bl.app_errorhandler(Exception)
 def handle_operation_failure_exception(e):
-
-    try:
-
-        log(os.getenv("LOGARBOR_SETTINGS_SERVICE_ID"), "critical", f"failed at: {request.path} and error: {str(e)}", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
-    except Exception as loge:
-
-        return {"message": f"{loge}"}, 500
     
     return {"message": "something went wrong"}, 500
 
