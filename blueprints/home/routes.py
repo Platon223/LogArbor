9c589254f2b7d0a354716b05c9f733f0e40074b4
Platon <platon.tikhnenko@gmail.com>
2026-01-25 16:18:46 -0600

    updated the code for the 0.10.2 client library to work

--- blueprints/home/routes.py
+++ blueprints/home/routes.py
@@ -13,11 +13,7 @@ home_blp = Blueprint("home_blp", __name__, template_folder="templates", static_f
 @home_blp.app_errorhandler(OperationFailure)
 def handle_operation_failure(e):
 
-    try:
-        log(os.getenv("LOGARBOR_HOME_SERVICE_ID"), "critical", f"failed db operation at: {request.path} and error: {e}", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
-    except Exception as loge:
-
-        return {"message": f"{loge}"}, 500
+    log(os.getenv("LOGARBOR_HOME_SERVICE_ID"), "critical", f"failed db operation at: {request.path} and error: {e}", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
     
     return {"message": "something went wrong"}, 500
 
@@ -28,12 +24,7 @@ def handle_operation_failure(e):
 @home_blp.app_errorhandler(PyMongoError)
 def handle_operation_failure_pymongo(e):
 
-    try:
-
-        log(os.getenv("LOGARBOR_HOME_SERVICE_ID"), "critical", f"failed db operation at: {request.path} and error: {e} because of a pymongo error", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
-    except Exception as loge:
-
-        return {"message": f"{loge}"}, 500
+    log(os.getenv("LOGARBOR_HOME_SERVICE_ID"), "critical", f"failed db operation at: {request.path} and error: {e} because of a pymongo error", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
     
     return {"message": "something went wrong"}, 500
 
@@ -41,7 +32,12 @@ def handle_operation_failure_pymongo(e):
 
 
 
+@home_blp.app_errorhandler(Exception)
+def handle_operation_failure_exception(e):
 
+    log(os.getenv("LOGARBOR_HOME_SERVICE_ID"), "critical", f"failed at: {request.path} and error: {e}", "ddcd3253-3d63-4254-9cbb-fc8531cef5f7")
+    
+    return {"message": "something went wrong"}, 500
 
 
 
