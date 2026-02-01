ab2ceacd0d42d579610ac6e58c422fe3d4d4fbd6
Platon <platon.tikhnenko@gmail.com>
2026-02-01 11:55:45 -0600

    added data validation to support bl

--- api/v1/support/routes.py
+++ api/v1/support/routes.py
@@ -5,6 +5,7 @@ import os
 from domains.service import check_api_blueprint, check_ui_blueprint
 from handlers.auth_check_wrapper import auth_check_wrapper
 from domains.support.service import send_feedback
+from validates.validate_api import validate_route
 
 
 support_bl = Blueprint("support_bl", __name__, template_folder="templates", static_folder="static")
@@ -37,6 +38,28 @@ def handle_operation_failure_exception(e):
     log(os.getenv("LOGARBOR_SUPPORT_SERVICE_ID"), "critical", f"failed at: {request.path} and error: {str(e)}", "5b522faa-76a4-444c-8253-7f045f5c06af")
     
     return {"message": "something went wrong"}, 500
+
+
+
+
+
+@support_bl.before_request
+def data_validation():
+
+    if request.method == "POST":
+
+        path = request.path
+
+        data = validate_route(request, path.removeprefix("/api/v1"))
+
+        if "error" in data:
+
+            log(os.getenv("LOGARBOR_SUPPORT_SERVICE_ID"), "warning", f"user failed data validation on api_validate on {path}", "5b522faa-76a4-444c-8253-7f045f5c06af")
+
+            return {"message": data}, 400
+        
+        g.data = data
+
 
 
 
