bf15724ed267bcf7527673de6ddc59baaa708f38
Platon <platon.tikhnenko@gmail.com>
2026-02-14 11:00:41 -0600

    trued auto detecting

--- extensions/socket.py
+++ extensions/socket.py
@@ -1,4 +1,4 @@
 from flask_socketio import SocketIO
 import os
 
-socketio = SocketIO(async_mode="gevent", message_queue="redis://localhost:6379/0")
+socketio = SocketIO(message_queue="redis://localhost:6379/0")
