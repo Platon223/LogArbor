05097e03455bb22d4abbbb3feb2226bb7152245e
Platon <platon.tikhnenko@gmail.com>
2026-02-14 11:57:12 -0600

    removed redis

--- extensions/limiter.py
+++ extensions/limiter.py
@@ -5,5 +5,5 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
    key_func=get_remote_address,
    default_limits=["100 per minute"],
-   storage_uri="redis://localhost:6379/1"
+   storage_uri="memory://"
 )
