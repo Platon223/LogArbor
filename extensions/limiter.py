f3235b6da3bd0386fe13d053bd89e7ee6cc32b60
Platon <platon.tikhnenko@gmail.com>
2026-02-22 11:33:24 -0600

    added local redis urls

--- extensions/limiter.py
+++ extensions/limiter.py
@@ -2,7 +2,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import os
 
-redis_storage_limiter = os.getenv("REDIS_STORAGE_LIMITER")
+redis_storage_limiter = os.getenv("REDIS_STORAGE_LIMITER", "redis://127.0.0.1:6379/2")
 
 limiter = Limiter(
    key_func=get_remote_address,
--- extensions/socket.py
+++ extensions/socket.py
@@ -1,6 +1,6 @@
 from flask_socketio import SocketIO
 import os
 
-redis_message_queue_socketio = os.getenv("REDIS_MESSAGE_QUEUE_SOCKETIO")
+redis_message_queue_socketio = os.getenv("REDIS_MESSAGE_QUEUE_SOCKETIO", "redis://127.0.0.1:6379/3")
 
 socketio = SocketIO(cors_allowed_origins="*", message_queue=redis_message_queue_socketio, async_mode="gevent")
