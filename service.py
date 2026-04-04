cc50fceec7e417e194b1069b1902cdfe6dcc6c18
Platon <platon.tikhnenko@gmail.com>
2026-04-04 12:25:00 -0500

    updated the jwt location

--- service.py
+++ service.py
@@ -32,7 +32,7 @@ def create_service():
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=1)
-    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
     app.config["JWT_ACCESS_COOKIE_NAME"] = "actk"
     app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 
